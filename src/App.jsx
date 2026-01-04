dc4451f9ef2ee48d1025b59f67c26c2b93e3866a
Norhan Hamed <62403829+norhanhamed@users.noreply.github.com>
2026-01-04 11:44:11 +0200

    adding hashRoter in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Hero from "../src/Components/Hero";
 import Skills from "../src/Components/Skills";
 import Projects from "./Components/Projects/Projects";
 import Contact from "./Components/Contact";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, HashRouter, RouterProvider } from "react-router-dom";
 import Layout from '../src/Components/Layout';
 import ReactProjects from "./Components/Projects/ReactProjects";
 import LandingProjects from "./Components/Projects/LandingProjects";
@@ -25,12 +25,16 @@ const routing = createBrowserRouter([
   }
 
 ],
- {
+  {
     basename: "/myportfolio",
   }
 )
 const App = () => {
-  return <RouterProvider router={routing} />;
+  return(
+  <HashRouter >
+    <RouterProvider router={routing} />;
+  </HashRouter>
+  )
 };
 
 export default App
