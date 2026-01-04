31045801cd44a7b41cdcf9ebe3df991b975fcab7
Norhan Hamed <62403829+norhanhamed@users.noreply.github.com>
2026-01-04 11:53:05 +0200

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import Hero from "../src/Components/Hero";
 import Skills from "../src/Components/Skills";
 import Projects from "./Components/Projects/Projects";
 import Contact from "./Components/Contact";
-import { createBrowserRouter, HashRouter, RouterProvider } from "react-router-dom";
+import {  createHashRouter, RouterProvider } from "react-router-dom";
 import Layout from '../src/Components/Layout';
 import ReactProjects from "./Components/Projects/ReactProjects";
 import LandingProjects from "./Components/Projects/LandingProjects";
 import Education from "./Components/Projects/Education";
 
-const routing = createBrowserRouter([
+const routing = createHashRouter ([
   {
     path: "/", element: <Layout />, children: [
       { index: true, element: <Hero /> },
@@ -25,16 +25,14 @@ const routing = createBrowserRouter([
   }
 
 ],
-  {
-    basename: "/myportfolio",
-  }
+  // {
+  //   basename: "/myportfolio",
+  // }
 )
 const App = () => {
-  return(
-  <HashRouter >
-    <RouterProvider router={routing} />;
-  </HashRouter>
-  )
+  return <RouterProvider router={routing} />;
+
+
 };
 
 export default App
